69ee1831dfb27df28464e7f0fc06fdb5d58d7a68
BasiaKon <bash@bash.>
2025-01-02 23:05:56 +0100

    podstawienie wsteczne

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,15 +1,26 @@
 #include "backsubst.h"
-/**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	if (mat->r != mat->c || mat->r != b->r || mat->r != x->r) {
+        return 2; // Błąd rozmiarów
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    int n = mat->r;
+	for (int i = n - 1; i >= 0; i--) {
+    	if (mat->data[i][i] == 0) {
+            return 1; // błąd dzielenia przez 0
+        }
+    double sum = b->data[i][0];
+
+        // Odejmuje sumę elementów w macierzy mat, które zostały już obliczone
+        for (int j = i + 1; j < n; j++) {
+            sum -= mat->data[i][j] * x->data[j][0];
+        }
+
+        // Obliczamy wartość zmiennej x[i]
+        x->data[i][0] = sum / mat->data[i][i];
+    }
+
+    return 0; 
 
 				int i;
 				for (i =0; i < x->r; i++) {
@@ -18,5 +29,3 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
 				return 0;
 }
-
-
